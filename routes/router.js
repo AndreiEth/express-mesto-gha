32345c953f84b6a51b312836af65f6cf599a54ae
Andrei Iurchenko <iur4.andrej@yandex.ru>
2023-06-05 21:22:07 +0300

    updated 4

--- routes/router.js
+++ routes/router.js
@@ -10,11 +10,11 @@ const {
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 
-router.use('/users', userRoutes);
-router.use('/cards', cardRoutes);
 router.post('/signin', loginValidation, login);
 router.post('/signup', createUserValidation, createUser);
 router.use(auth);
+router.use('/users', userRoutes);
+router.use('/cards', cardRoutes);
 router.use('/*', (req, res, next) => {
   next(new NotFoundError('This page does not exist'));
 });
